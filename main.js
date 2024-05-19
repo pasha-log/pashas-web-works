7c5e6689fa74afc1bb08841fceae694be2ea1610
pasha-log <pasha.log@gmail.com>
2024-05-19 02:41:48 -0700

    Followed along a Fireship tutorial to understand the basics of three.js, and created a temporary boilerplate for my portfolio

--- main.js
+++ main.js
@@ -1,24 +1,121 @@
 import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Setup
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.querySelector('#bg')
+});
 
-const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+camera.position.setZ(30);
+camera.position.setX(-3);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+renderer.render(scene, camera);
 
-camera.position.z = 5;
+// Torus
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+// Light
+
+const pointLight = new THREE.PointLight(0xffffff, 20, 5000);
+pointLight.position.set(0, 0, 2);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+// Add stars
+
+function addStar() {
+	const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+	const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	const star = new THREE.Mesh(geometry, material);
+
+	const [ x, y, z ] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+	star.position.set(x, y, z);
+	scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+// Add Background
+
+const spaceTexture = new THREE.TextureLoader().load('./public/space.jpg');
+scene.background = spaceTexture;
+
+// Add Pasha
+
+const pashaTexture = new THREE.TextureLoader().load('./public/pasha-bw.jpg');
+const pasha = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: pashaTexture }));
+
+scene.add(pasha);
+
+// Add Mars
+
+const marsTexture = new THREE.TextureLoader().load('./public/mars.jpeg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpeg');
+
+const mars = new THREE.Mesh(
+	new THREE.SphereGeometry(3, 32, 32),
+	new THREE.MeshStandardMaterial({
+		map: marsTexture,
+		normalMap: normalTexture
+	})
+);
+
+scene.add(mars);
+
+mars.position.z = 30;
+mars.position.setX(-10);
+
+// Move Camera on Scroll
+
+function moveCamera() {
+	const t = document.body.getBoundingClientRect().top;
+	mars.rotation.x += 0.05;
+	mars.rotation.y += 0.075;
+	mars.rotation.z += 0.05;
+
+	pasha.rotation.y += 0.01;
+	pasha.rotation.z += 0.01;
+
+	camera.position.z = t * -0.01;
+	camera.position.x = t * -0.0002;
+	camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Controls
 
 function animate() {
 	requestAnimationFrame(animate);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+
+	torus.rotation.x += 0.01;
+	torus.rotation.y += 0.005;
+	torus.rotation.z += 0.01;
+
+	mars.rotation.x += 0.005;
+
+	// controls.update();
+
 	renderer.render(scene, camera);
 }
 animate();
